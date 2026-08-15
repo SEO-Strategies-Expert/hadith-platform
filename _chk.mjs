5725d3e7064390b535cc985ea130375e5c4b8731
SEO-Strategies-Expert <hassan67844@gmail.com>
2026-08-15 12:52:25 +0300

    إزالة ملف فحص مؤقّت

--- _chk.mjs
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const p = new PrismaClient();
-console.log("admission_applications rows =", await p.admissionApplication.count());
-await p.$disconnect();
